c756e6a2eeaeb37873d5e77dc1956cedc4741acc
tawfiq <tawfiq_kh@yahoo.com>
2021-06-14 12:44:24 +0300

    solves most

--- src/EmployeeMatrix.js
+++ src/EmployeeMatrix.js
@@ -1,12 +1,32 @@
-/* Write your code below */
+let Matrix = require('./Matrix')
 
 
+class EmployeeMatrix extends Matrix {
+    constructor(row, col) {
+        super(row, col);
+    }
 
+    loadData(salaryData) {
+        let convertobjToMatrix = []
+        for (let i = 0; i < salaryData.length; i++) {
+            convertobjToMatrix.push(Object.values(salaryData[i]));
+        }
+        this.matrix = convertobjToMatrix;
+    }
+}
 
-//You can paste the code from the lesson below to test your solution
+let data = [
+    { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
+    { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
+    { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
+    { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
+    { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
+    { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 }
+]
 
+let m = new EmployeeMatrix()
 
+m.loadData(data)
+m.print()
 
-
-/* Do not remove the exports below */
 module.exports = EmployeeMatrix
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,12 +1,77 @@
-/* Write your code below */
+class Matrix {
+    constructor(rows, cols) {
+        this.matrix = this.generateMatrix(rows, cols)
+    }
 
+    generateMatrix(rows, cols) {
+        let counter = 1;
+        let matrix = [];
+        for (let row = 0; row < rows; row++) {
+            let row = [];
+            for (let col = 0; col < cols; col++) {
+                row.push(counter++);
+            }
+            matrix.push(row)
+        }
+        return matrix;
+    }
+    print() {
+        for (let row = 0; row < this.matrix.length; row++) {
+            let line = "";
+            for (let col = 0; col < this.matrix[row].length; col++) {
+                line += this.matrix[row][col] + "\t";
+            }
+            console.log(line);
+        }
+    }
 
+    get(row, col) {
+        return this.matrix[row][col];
+    }
 
+    alter(row, col, val) {
+        this.matrix[row][col] = val;
+    }
 
-//You can paste the code from the lesson below to test your solution
+    printColumn(col) {
+        for (let row = 0; row < this.matrix.length; row++) {
+            console.log(this.matrix[row][col]);
+        }
+    }
 
+    printRow(row) {
+        for (let col = 0; col < this.matrix[row].length; col++) {
+            console.log(this.matrix[row][col]);
+        }
+    }
 
+    findCoordinate(val) {
 
+        for (let row = 0; row < this.matrix.length; row++) {
+            let col = this.matrix[row].findIndex(elem => elem == val);
+            if (col > 0) {
+                return { x: col, y: row };
+            }
+        }
+    }
+}
 
-/* Do not remove the exports below */
+let m = new Matrix(3, 4)
+    // m.print()
+    //prints
+    /*
+    1       2       3       4
+    5       6       7       8
+    9       10      11      12
+    */
+
+// console.log("");
+// m.alter(0, 0, m.get(1, 1))
+// m.printColumn(0) //prints 6, 5, 9 (separate lines)
+// console.log("");
+// m.printRow(0) //prints 6, 2, 3, 4 (separate lines)
+
+// console.log(m.findCoordinate(12)) //prints {x: 3, y: 2}
+// console.log("");
+// console.log(m.findCoordinate(7)) //prints {x: 2, y: 1}
 module.exports = Matrix
